f4940d08ac7bf53e0954c4d0c88485762b880e81
Sothyra Chan <schan256@my.centennialcollege.ca>
2024-11-26 23:46:34 -0500

    added more regex variables

--- script.js
+++ script.js
@@ -8,10 +8,16 @@ const checkMessageButton = document.getElementById("check-message-btn");
 const helpRegex = /please help|assist me/i;
 
 // plus sign (+) matches the preceding element one or more times e.g. [0-9]+ matches one or more digits
-const dollarRegex = /[0-9]+ hundred|thousand|million|billion dollars/i;
+const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i;
+
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
+
+const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
+
+const dearRegex = /dear friend/i;
 
 // check more than one regex
-const denyList = [helpRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex];
 
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg)); // This will return true if at least one of the regex in the denyList array is found in the message
 // const isSpam = (msg) => helpRegex.test(msg);
